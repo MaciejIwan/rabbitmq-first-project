1e4fc83c157aa9fd6a43529d85796d01a5566721
Maciej Iwan <64867638+MaciejIwan@users.noreply.github.com>
2024-06-02 13:02:32 +0200

    init

---
+++ src/main/java/com/miwan/course/rabbitmq/rabbitmqcourse/RabbitMqCourseApplication.java
@@ -0,0 +1,13 @@
+package com.miwan.course.rabbitmq.rabbitmqcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miwan/course/rabbitmq/rabbitmqcourse/RabbitMqCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miwan.course.rabbitmq.rabbitmqcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
